09124a3ffaa3d68778700e1c4cdbe23cf8bc89c2
maksim-mshp <maksim-mshp@ya.ru>
2020-12-24 19:26:27 +0300

    Created Pizza.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Parent: {{pizzaName}}</h1>
+    <app-pizza v-bind:pizzaName='pizzaName' v-bind:pizzaPrice='pizzaPrice'>
+      <h1> С Новым годом! </h1>
+      <h2 slot='n1'>Покупайте пиццу</h2>
+    </app-pizza>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Pizza from './components/Pizza.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    data() {
+      return {
+        pizzaName: 'ШПицца от Школы программистов',
+        pizzaPrice: 600
+
+      }
+    },
+    components: {
+      appPizza: Pizza
+    }
   }
-}
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/Pizza.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h2> Pizza Name: {{pizzaName}}</h2>
+        <p>Price: {{pizzaPrice}}</p>
+        <slot name='n1'> </slot>
+        <slot> </slot>
+    </div>
+</template>
+<script>
+    export default {
+        props: ['pizzaName', 'pizzaPrice']
+    }
+</script>
